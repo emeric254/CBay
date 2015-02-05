157186bf21e494d3a66017c22ed77378cb362a94
emeric254 <emeric254@live.fr>
2015-02-05 15:24:04 +0100

    clean up of this file 	modified:   mainServeur.c

--- sources/mainServeur.c
+++ sources/mainServeur.c
@@ -12,6 +12,7 @@ int main()
     int length = 0;
     int end = FALSE;
     int quit = FALSE;
+    int connected = FALSE;
 
     Init(SERVER_PORT);
 
@@ -29,6 +30,7 @@ int main()
                 if(isConnectRequest(message, length) == TRUE)
                 {
                     // work with this Connect request
+                    // connected = TRUE;
                 }
                 else if(isDeleteRequest(message, length) == TRUE)
                 {
@@ -83,48 +85,6 @@ int main()
             return SUCESS;
         }
     }
-
-    /*
-        while(!fini) // on garde cette boucle au cas ou la requete "GET [...]" ne soit pas la premiere ligne envoyee par le client
-        {
-/////
-            if(message != NULL)
-            {
-                if(extraitFichier(message, &nomFichier, &tailleNomFichier))
-                {
-                    if(tailleNomFichier<1)
-                        retour = envoyerReponse400(); // BAD_REQUEST
-                    else
-                    {
-                        if(!strlen(nomFichier) || !strcmp(nomFichier,"HTTP/1.1")) // pas de nom nomfichier ou '/' ?
-                        {
-                            free(nomFichier);
-                            nomFichier=malloc(11*sizeof(char));
-                            if(nomFichier!=NULL) strcpy(nomFichier,"index.html"); // on envoie la page d'accueil !
-                        }
-
-                        if(longueur_fichier(nomFichier)!=-1) // si existe
-                        {
-                            if(testExtension(nomFichier,".jpg"))
-                                retour = envoyerReponse200JPG(nomFichier);
-                            else if(testExtension(nomFichier,".ico"))
-                                retour = envoyerReponse200ICO(nomFichier);
-                            else if(testExtension(nomFichier,".html"))
-                                retour = envoyerReponse200HTML(nomFichier);
-                            else retour = envoyerReponse403(); // FORBIDDEN
-                        }
-                        else
-                            retour = envoyerReponse404(); // NOT_FOUND
-                    }
-
-                    if(!retour)
-                        envoyerReponse500(); // SERVER_INTERNAL_ERROR
-                    retour = 0;
-                    fini = 1;
-                }
-            } else  fini = 1;
-        }
-    */
 
     return ERROR_UNKNOWN;
 }
